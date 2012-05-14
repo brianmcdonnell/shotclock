46000269942882a408d59bc4c3c05a2b216b2bbb
Brian McDonnell <mcdonnellb@gmail.com>
2012-05-14 23:31:29 +0100

    Initial implementation.

---
+++ shotclock.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os
+import glob
+import mimetypes
+
+def create_backup(input_path):
+    dir_path, filename = os.path.split(input_path)
+    bkp_dir = os.path.join(dir_path, 'bkp')
+
+    # Don't proceed unless we can backup the files
+    if os.path.isfile(bkp_dir):
+        raise Exception("File exists at backup dir location.")
+    if not os.path.exists(bkp_dir):
+        os.makedirs(bkp_dir)
+
+    # Backup the file
+    bkp_path = os.path.join(bkp_dir, filename)
+    if os.path.isfile(bkp_path):
+        raise Exception("Backup file already exists.  Will not overwrite.")
+    import shutil
+    shutil.copyfile(input_path, bkp_path)
+
+def process_file(path, command):
+    # Backup the file before it's processed
+    #create_backup(path)
+
+    fulltype = mimetypes.guess_type(path)
+    maintype, subtype = fulltype[0].split('/')
+    if (fulltype[0] == 'image/pjpeg'):
+        command.process_jpeg(path)
+    elif (fulltype[0] == 'video/x-msvideo'):
+        command.process_avi(path)
+    elif maintype == "video":
+        command.process_avi(path)
+
+def process_expanded_arg(arg, command):
+    ''' We want to process lists of file and directory arguments.
+        We don't want to recurse directories.
+    '''
+    if os.path.isfile(arg):
+        process_file(arg, command)
+    elif os.path.isdir(arg):
+        for p in os.listdir(arg):
+            p = os.path.join(arg, p)
+            if os.path.isfile(p):
+                process_file(p, command)
+
+def main():
+    ''' canon.py (timeshift|rename) [options] path1 path2... 
+    '''
+    import sys
+    if sys.argv[1] == 'timeshift':
+        from timeshifter import TimeShifter
+        command = TimeShifter()
+    elif sys.argv[1] == 'rename':
+        from renamer import Renamer
+        command = Renamer()
+    else:
+        print "Canon Media Helper"
+        print ""
+        print "Commands:"
+        print "  timeshift\t\ttranspose timestamps by specified amount"
+        print "  rename   \t\trename files from metadata"
+        exit(1)
+
+    for arg in command.arguments:
+        for expanded in glob.glob(arg):
+            process_expanded_arg(expanded, command)
+
+if __name__ == '__main__':
+    main()
+
---
+++ timeshifter.py
@@ -0,0 +1,109 @@
+from sys import stderr, exit
+import datetime
+
+class TimeShifter(object):
+    usage = "%prog timeshift [options] path"
+    date_format = '%a %b %d %H:%M:%S %Y\n'
+    output_dir = 'output'
+
+    def __init__(self):
+        import optparse
+        parser = optparse.OptionParser(TimeShifter.usage)
+        parser.add_option('--hours', '-H', dest='hours', default=None)
+        parser.add_option('--minutes', '-M', dest='minutes', default=None)
+        self.options, self.arguments = parser.parse_args()
+        if not self.options.hours and not self.options.minutes:
+            parser.error("Either 'hours' or 'minutes' must be specified")
+
+    def show_usage(self):
+        print TimeShifter.usage
+
+    def _get_output_path(self, input_path):
+        import os
+        dirname, filename = os.path.split(input_path)
+        output_dir = os.path.join(dirname, TimeShifter.output_dir)
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(output_dir, filename)
+        return unicode(output_path), str(output_path)
+
+    def process_jpeg(self, input_path):
+        uoutput_path, output_path = self._get_output_path(input_path)
+        import shutil
+        shutil.copyfile(input_path, output_path)
+
+        import pyexiv2
+        metadata = pyexiv2.ImageMetadata(output_path)
+        metadata.read()
+        dt = metadata['Exif.Image.DateTime'].value
+
+        # Replace exif dates with shifted date
+        shifted_dt = self._shift_time(dt)
+        metadata['Exif.Image.DateTime'].value = shifted_dt
+        metadata['Exif.Photo.DateTimeOriginal'].value = shifted_dt
+        metadata['Exif.Photo.DateTimeDigitized'].value = shifted_dt
+
+        # Write output file
+        metadata.write()
+
+    def process_avi(self, input_path):
+        # Try parsing the file
+        from hachoir_parser import createParser
+        from hachoir_core.cmd_line import unicodeFilename
+        uinput_path = unicodeFilename(input_path)
+        parser = createParser(uinput_path, input_path)
+        if not parser:
+            print >> stderr, "Unable to parse file"
+            exit(1)
+
+        # Extract metadata from the file
+        from hachoir_metadata import extractMetadata
+        from hachoir_core.error import HachoirError
+        try:
+            metadata = extractMetadata(parser)
+        except HachoirError, err:
+            print >> stderr, "Metadata extraction error: %s" % unicode(err)
+            metadata = None
+        if not metadata:
+            print >> stderr, "Unable to extract metadata"
+            exit(1)
+
+        # Can rewrite the datetime here easily if the new datetime string length
+        # is the same as the old datetime string length.
+        # If the length were to change, we'd have to update various chunk length values
+        # e.g. file size, header size and datetime size
+        from hachoir_editor import createEditor
+        editor = createEditor(parser)
+        headers_fieldset = editor['headers']
+        datetime_fieldset = headers_fieldset['datetime']
+
+        # Chop off the last character
+        original_datetime_str = datetime_fieldset['text'].value
+        #original_datetime_str = original_datetime_str[:-1]
+        original_datetime = datetime.datetime.strptime(original_datetime_str, TimeShifter.date_format)
+
+        # Shift time as specified in args
+        shifted_time = self._shift_time(original_datetime)
+        shifted_time_str = shifted_time.strftime(TimeShifter.date_format).upper()
+        shifted_time_str += '\0'
+
+        # Modify the metadata fields
+        datetime_fieldset['text'].value = shifted_time_str
+        datetime_fieldset['size'].value = len(shifted_time_str)
+
+        # Write out the file
+        uoutput_path, output_path = self._get_output_path(input_path)
+        from hachoir_core.stream import FileOutputStream
+        output = FileOutputStream(uoutput_path, output_path)
+        editor.writeInto(output)
+
+        # Close the file io stream
+        parser.stream._input.close()
+
+    def _shift_time(self, input_time):
+        hrs = int(self.options.hours) if self.options.hours else 0
+        mins = int(self.options.minutes) if self.options.minutes else 0
+        td = datetime.timedelta(hours=hrs, minutes=mins)
+        output_time = input_time + td
+        print "%s -> %s (h:%s m:%s)" % (input_time, output_time, hrs, mins)
+        return output_time
