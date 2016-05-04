5b422e8cebc5fd2e4d18f28c444424575e5e167d
Brian McDonnell <mcdonnellb@gmail.com>
2016-05-05 00:12:46 +0100

    Find create/modify dates in track and media metadata sections.

--- fileformats/mov.py
+++ fileformats/mov.py
@@ -13,6 +13,7 @@ class MOVFile(BaseFile):
         if not self.parser:
             raise Exception("Could not parse: %s" % path)
         self._new_date = None
+        self.inspect()
 
     def get_date(self):
         from hachoir_metadata import extractMetadata
@@ -41,6 +42,34 @@ class MOVFile(BaseFile):
         from hachoir_core.stream import FileOutputStream
         output = FileOutputStream(path)
         editor.writeInto(output)
+
+    def _find_atom_by_field(self, node, field_name):
+        for atom in node.array('atom'):
+            print [x for x in atom._fields.iterkeys()]
+            if field_name in atom:
+                 # print "Found:", field_name
+                return atom[field_name]
+        # print "Not found:", field_name
+        return None
+
+    def inspect(self):
+        movie = self._find_atom_by_field(self.parser, 'movie')
+        movie_hdr = self._find_atom_by_field(movie, 'movie_hdr')
+        # import pdb;pdb.set_trace()
+        print movie_hdr['creation_date']
+        print movie_hdr['lastmod_date']
+
+        print "--- track ---"
+        track = self._find_atom_by_field(movie, 'track')
+        track_hdr = self._find_atom_by_field(track, 'track_hdr')
+        print track_hdr['creation_date']
+        print track_hdr['lastmod_date']
+
+        print "--- media ---"
+        media = self._find_atom_by_field(track, 'media')
+        media_hdr = self._find_atom_by_field(media, 'media_hdr')
+        print media_hdr['creation_date']
+        print media_hdr['lastmod_date']
 
     def close(self):
         self.parser.stream._input.close()
